1187ddbf38f4ffcb3f74266c5313ddd67681d4de
mohamed magdy <mohamed.magdy.official2@gmail.com>
2026-06-05 16:25:10 +0300

    creating booking entity

---
+++ Ehgiz.DAL/Entities/Booking.cs
@@ -0,0 +1,10 @@
+public class Booking
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int RoomId { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    // Navigation properties
+   
+}
